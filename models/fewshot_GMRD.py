3659c76e12aeccfef22bb899b62a4d593efe74b2
Ziming Cheng <126794842+zmcheng9@users.noreply.github.com>
2023-11-02 15:26:57 +0800

    Add files via upload

--- models/fewshot_GMRD.py
+++ models/fewshot_GMRD.py
@@ -56,13 +56,12 @@ class FewShotSeg(nn.Module):
         img_size = supp_imgs[0][0].shape[-2:]
         supp_mask = torch.stack([torch.stack(way, dim=0) for way in supp_mask],
                                 dim=0).view(supp_bs, self.n_ways, self.n_shots, *img_size)  # B x Wa x Sh x H x W
-        # print(supp_mask.size())  #  (1, 1, 1, 256,256)
 
         ###### Extract features ######
         imgs_concat = torch.cat([torch.cat(way, dim=0) for way in supp_imgs]
                                 + [torch.cat(qry_imgs, dim=0), ], dim=0)      # (2, 3, 256,256)
         # encoder output
-        img_fts, tao = self.encoder(imgs_concat)  #(2, 512, 32, 32) (2, 512, 64, 64)   (2, 1)
+        img_fts, tao = self.encoder(imgs_concat)
         supp_fts = [img_fts[dic][:self.n_ways * self.n_shots * supp_bs].view(  # B x Wa x Sh x C x H' x W'
             supp_bs, self.n_ways, self.n_shots, -1, *img_fts[dic].shape[-2:]) for _, dic in enumerate(img_fts)]
         supp_fts = supp_fts[0]
@@ -72,44 +71,43 @@ class FewShotSeg(nn.Module):
         qry_fts = qry_fts[0]
 
         ##### Get threshold #######
-        self.t = tao[self.n_ways * self.n_shots * supp_bs:]  # t for query features   [[t]] (1, 1)
-        self.thresh_pred = [self.t for _ in range(self.n_ways)]  # [tensor([[t]])]
+        self.t = tao[self.n_ways * self.n_shots * supp_bs:]
+        self.thresh_pred = [self.t for _ in range(self.n_ways)]
 
         ###### Compute loss ######
         align_loss = torch.zeros(1).to(self.device)
-        mse_loss = torch.zeros(1).to(self.device)
+        aux_loss = torch.zeros(1).to(self.device)
         outputs = []
         for epi in range(supp_bs):
             ###### Extract prototypes ######
             if supp_mask[epi][0].sum() == 0:
-                supp_fts_ = [[self.getFeatures(supp_fts[[epi], way, shot], supp_mask[[epi], way, shot])  # [1, 512, 64, 64]  [1, 256, 256]
-                               for shot in range(self.n_shots)] for way in range(self.n_ways)]   # every fig prototype (1, 512) (2, 1, 1, tensor(1, 512))
-                fg_prototypes = self.getPrototype(supp_fts_)  # the prototypes for support images (2,1,(1, 512))
+                supp_fts_ = [[self.getFeatures(supp_fts[[epi], way, shot], supp_mask[[epi], way, shot])
+                               for shot in range(self.n_shots)] for way in range(self.n_ways)]
+                fg_prototypes = self.getPrototype(supp_fts_)
 
                 ###### Get query predictions ######
-                qry_pred = torch.stack(   # (1, 512, 64, 64) (1, 512) (1, 1)
-                    [self.getPred(qry_fts[epi], fg_prototypes[way], self.thresh_pred[way])       # (2, (1, 1, 64,64)) (2, (N, way, 64,64))
+                qry_pred = torch.stack(
+                    [self.getPred(qry_fts[epi], fg_prototypes[way], self.thresh_pred[way])
                      for way in range(self.n_ways)], dim=1) # 2 x N x Wa x H' x W'
                 preds = F.interpolate(qry_pred, size=img_size, mode='bilinear', align_corners=True)
-                preds = torch.cat((1.0 - preds, preds), dim=1)  # (1, 2, 256, 256)
+                preds = torch.cat((1.0 - preds, preds), dim=1)
                 outputs.append(preds)
                 if train:
                     align_loss_epi = self.alignLoss([supp_fts[epi]], [qry_fts[epi]], preds, supp_mask[epi])
                     align_loss += align_loss_epi
             else:
                 fg_pts = [[self.get_fg_pts(supp_fts[[epi], way, shot], supp_mask[[epi], way, shot])
-                                   for shot in range(self.n_shots)] for way in range(self.n_ways)] # (2, way, shot, (102, 256))
-                fg_pts = self.get_all_prototypes(fg_pts)     # (2, way, (102, 512)) lsit of list of tensor [(102, 512)]
+                                   for shot in range(self.n_shots)] for way in range(self.n_ways)]
+                fg_pts = self.get_all_prototypes(fg_pts)     # lsit of list of tensor [(102, 512)]
 
                 bg_pts = [[self.get_bg_pts(supp_fts[[epi], way, shot], supp_mask[[epi], way, shot])
-                                   for shot in range(self.n_shots)] for way in range(self.n_ways)]  # (2, way, shot, (102, 256))
-                bg_pts = self.get_all_prototypes(bg_pts)    # (2, way, (402, 512)) lsit of list of tensor [(402, 512)]
+                                   for shot in range(self.n_shots)] for way in range(self.n_ways)]
+                bg_pts = self.get_all_prototypes(bg_pts)    # lsit of list of tensor
 
                 ###### Get query predictions ######
-                fg_sim = torch.stack(  # (1, 512, 64, 64) (200, 512)
+                fg_sim = torch.stack(
                     [self.get_fg_sim(qry_fts[epi], fg_pts[way]) for way in range(self.n_ways)], dim=1).squeeze(0)
-                # print(fg_sim[0].size(), 'asdasd')
-                bg_sim = torch.stack(  # (1, 512, 64, 64) (200, 512)
+                bg_sim = torch.stack(
                     [self.get_bg_sim(qry_fts[epi], bg_pts[way]) for way in range(self.n_ways)], dim=1).squeeze(0)
 
                 preds = F.interpolate(fg_sim, size=img_size, mode='bilinear', align_corners=True)
@@ -120,16 +118,16 @@ class FewShotSeg(nn.Module):
 
                 outputs.append(preds)
                 if train:
-                    align_loss_epi, mse_loss_epi = self.align_mse_Loss([supp_fts[epi]], [qry_fts[epi]], preds,
+                    align_loss_epi, aux_loss_epi = self.align_aux_Loss([supp_fts[epi]], [qry_fts[epi]], preds,
                                                          supp_mask[epi], fg_pts, bg_pts)  # fg_pts, bg_pts
                     align_loss += align_loss_epi
-                    mse_loss += mse_loss_epi
+                    aux_loss += aux_loss_epi
 
 
         output = torch.stack(outputs, dim=1)  # N x B x (1 + Wa) x H x W
         output = output.view(-1, *output.shape[2:])
 
-        return output, align_loss / supp_bs, mse_loss / supp_bs
+        return output, align_loss / supp_bs, aux_loss / supp_bs
 
 
     def getPred(self, fts, prototype, thresh):
@@ -221,7 +219,7 @@ class FewShotSeg(nn.Module):
 
         return loss
 
-    def align_mse_Loss(self, supp_fts, qry_fts, pred, fore_mask, sup_fg_pts, sup_bg_pts):   # [(102, 512)], [(402, 512)]
+    def align_aux_Loss(self, supp_fts, qry_fts, pred, fore_mask, sup_fg_pts, sup_bg_pts):
         """
             supp_fts: [1, 512, 64, 64]
             qry_fts: (1, 512, 64, 64)
@@ -239,7 +237,7 @@ class FewShotSeg(nn.Module):
 
         # Compute the support loss
         loss = torch.zeros(1).to(self.device)
-        loss1 = torch.zeros(1).to(self.device)
+        loss_aux = torch.zeros(1).to(self.device)
         for way in range(n_ways):
             if way in skip_ways:
                 continue
@@ -247,16 +245,11 @@ class FewShotSeg(nn.Module):
             for shot in range(n_shots):
                 # Get prototypes         1 x C x H' x W'   1 x H x W
                 qry_fts_ = [self.get_fg_pts(qry_fts[0], pred_mask[way + 1])]
-                fg_prototypes = self.get_all_prototypes([qry_fts_])  # [(102, 512)]
-                qry_fg_pts = self.get_avg_pts(fg_prototypes[way])
-                supp_fg_pts = self.get_avg_pts(sup_fg_pts[way])
-                loss1 += self.criterion_MSE(supp_fg_pts, qry_fg_pts)
-
+                fg_prototypes = self.get_all_prototypes([qry_fts_])
                 bg_pts_ = [self.get_bg_pts(qry_fts[0], pred_mask[way + 1])]
-                bg_pts_ = self.get_all_prototypes([bg_pts_])   # [(402, 256)]
-                qry_bg_pts = self.get_avg_pts(bg_pts_[way])
-                supp_bg_pts = self.get_avg_pts(sup_bg_pts[way])
-                loss1 += self.criterion_MSE(supp_bg_pts, qry_bg_pts)
+                bg_pts_ = self.get_all_prototypes([bg_pts_])
+
+                loss_aux += self.get_aux_loss(sup_fg_pts[way], fg_prototypes[way], sup_bg_pts[way], bg_pts_[way])
 
                 # Get predictions
                 supp_pred = self.get_fg_sim(supp_fts[0][way, [shot]], fg_prototypes[way])   # N x Wa x H' x W'
@@ -278,7 +271,7 @@ class FewShotSeg(nn.Module):
                 log_prob = torch.log(torch.clamp(preds, eps, 1 - eps))
                 loss += self.criterion(log_prob, supp_label[None, ...].long()) / n_shots / n_ways
 
-        return loss, loss1
+        return loss, loss_aux
 
 
     def get_fg_pts(self, features, mask):
@@ -336,10 +329,13 @@ class FewShotSeg(nn.Module):
             k = random.sample(range(len(features_trans)), n_protptype)
             prototypes = features_trans[k]
         else:
-            r = (n_protptype) // len(features_trans)
-            k = random.sample(range(len(features_trans)), (n_protptype - len(features_trans)) % len(features_trans))
-            prototypes = torch.cat([features_trans for _ in range(r)], dim=0)
-            prototypes = torch.cat([features_trans[k], prototypes], dim=0)
+            if len(features_trans) == 0:
+                prototypes = torch.zeros(n_protptype, 512).to(self.device)
+            else:
+                r = (n_protptype) // len(features_trans)
+                k = random.sample(range(len(features_trans)), (n_protptype - len(features_trans)) % len(features_trans))
+                prototypes = torch.cat([features_trans for _ in range(r)], dim=0)
+                prototypes = torch.cat([features_trans[k], prototypes], dim=0)
 
         return prototypes  # (n_prototype, 512)
 
@@ -405,15 +401,33 @@ class FewShotSeg(nn.Module):
 
         return bg_sim  # [1, 1, 64, 64]
 
-    def get_avg_pts(self, pts):
-        """
-        Args:
-            pts: (102, 512)
-        """
-        avg_pts = torch.mean(pts[:-2], dim=0).unsqueeze(0)
-        prototypes = torch.cat([avg_pts, pts[-2:]], dim=0)
+    def get_aux_loss(self, sup_fg_pts, qry_fg_pts, sup_bg_pts, qry_bg_pts):
+        d1 = F.normalize(sup_fg_pts, dim=-1)
+        d2 = F.normalize(qry_fg_pts, dim=-1)
+        b1 = F.normalize(sup_bg_pts, dim=-1)
+        b2 = F.normalize(qry_bg_pts, dim=-1)
 
-        return prototypes
+        fg_intra0 = torch.matmul(d1[:-2], d2[:-2].transpose(0, 1))
+        fg_intra1 = torch.matmul(d1[[-2]], d2[-2])
+        fg_intra2 = torch.matmul(d1[[-1]], d2[-1])
+        fg_intra0, _ = torch.max(fg_intra0, dim=1)
+        fg_intra0 = torch.cat([fg_intra0, fg_intra1, fg_intra2])
+        fg_intra0 = torch.mean(fg_intra0)
+
+        bg_intra0 = torch.matmul(b1[:-2], b2[:-2].transpose(0, 1))
+        bg_intra1 = torch.matmul(b1[[-2]], b2[-2])
+        bg_intra2 = torch.matmul(b1[[-1]], b2[-1])
+        bg_intra0, _ = torch.max(bg_intra0, dim=1)
+        bg_intra0 = torch.cat([bg_intra0, bg_intra1, bg_intra2])
+        bg_intra0 = torch.mean(bg_intra0)
+        intra_loss = 2 - fg_intra0 - bg_intra0
+
+        sup_inter = torch.matmul(d1, b1.transpose(0, 1))
+        qry_inter = torch.matmul(d2, b2.transpose(0, 1))
+        inter_loss = max((0, torch.mean(sup_inter))) + max((0, torch.mean(qry_inter)))
+
+        return intra_loss + inter_loss
+
 
 
 
